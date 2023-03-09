79348febd8c7f2a463fb82cfd1e8a63b6f58611c
Eric Wittmann <eric.wittmann@gmail.com>
2023-03-09 14:17:22 -0500

    Initial conversion from pnpm to npm, with PF seed for the webapp

---
+++ ui/apps/web/webpack.dev.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const {merge} = require("webpack-merge");
+const CopyPlugin = require("copy-webpack-plugin");
+const common = require("./webpack.common.js");
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || "9000";
+const PROTOCOL = process.env.PROTOCOL || "http";
+
+module.exports = merge(common("development"), {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        https: PROTOCOL === "https",
+        host: HOST,
+        port: PORT,
+        historyApiFallback: true,
+        open: true,
+        hot: true,
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        client: {
+            overlay: true,
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+        },
+    },
+    // plugins: [
+    //     new CopyPlugin({
+    //         patterns: [
+    //             {from: "./src/keycloak.dev.json", to: "keycloak.json"}
+    //         ]
+    //     })
+    // ]
+});
