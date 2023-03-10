ba23b6587ab909c396bdf5c3f7d970c04fb64f44
Jakub Senko <m@jsenko.net>
2023-03-10 12:40:35 +0100

    feat: bootstrap basic application structure, with a REST API

---
+++ app/src/main/java/io/apicurio/designer/storage/AbstractSqlStorage.java
@@ -0,0 +1,9 @@
+package io.apicurio.designer.storage;
+
+import io.apicurio.designer.spi.storage.DesignerStorage;
+
+/**
+ * @author Jakub Senko <m@jsenko.net>
+ */
+public abstract class AbstractSqlStorage implements DesignerStorage {
+}
