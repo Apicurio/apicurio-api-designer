85cb806ec7fead42f7774068f00378621dd1d12e
Eric Wittmann <eric.wittmann@gmail.com>
2023-03-20 11:53:26 -0400

    Fixed some warnings in the java code

--- spi/src/main/java/io/apicurio/designer/spi/storage/DesignNotFoundException.java
+++ spi/src/main/java/io/apicurio/designer/spi/storage/DesignNotFoundException.java
@@ -2,6 +2,8 @@ package io.apicurio.designer.spi.storage;
 
 public class DesignNotFoundException extends StorageException {
 
+    private static final long serialVersionUID = 3153648388051440385L;
+
     public DesignNotFoundException(String message) {
         super(message);
     }
