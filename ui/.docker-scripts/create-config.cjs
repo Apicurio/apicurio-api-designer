1832e7fcb68dba2dc7a609bec98a382379e31426
Eric Wittmann <eric.wittmann@gmail.com>
2024-03-19 09:46:18 -0400

    Updated config properties to be prefixed by "apicurio." instead of "designer."

--- ui/.docker-scripts/create-config.cjs
+++ ui/.docker-scripts/create-config.cjs
@@ -2,26 +2,26 @@
 
 const fs = require("fs");
 
-const CONFIG_OUTPUT_PATH=process.env["DESIGNER_CONFIG_OUTPUT_PATH"] || "/opt/app-root/src/config.js";
+const CONFIG_OUTPUT_PATH=process.env["APICURIO_CONFIG_OUTPUT_PATH"] || "/opt/app-root/src/config.js";
 
 console.info("Generating application config at:", CONFIG_OUTPUT_PATH);
 
-const CONTEXT_PATH=process.env["DESIGNER_CONTEXT_PATH"] || "/";
-const NAV_PREFIX_PATH=process.env["DESIGNER_NAV_PREFIX_PATH"] || "";
-const DESIGNER_API_URL=process.env["DESIGNER_API_URL"] || "/apis/designer/v0";
-const SHOW_MASTHEAD=process.env["DESIGNER_SHOW_MASTHEAD"] || "true";
-const MASTHEAD_LABEL=process.env["DESIGNER_MASTHEAD_LABEL"] || "API DESIGNER";
-const EDITORS_URL=process.env["DESIGNER_EDITORS_URL"] || "/editors/";
+const CONTEXT_PATH=process.env["APICURIO_CONTEXT_PATH"] || "/";
+const NAV_PREFIX_PATH=process.env["APICURIO_NAV_PREFIX_PATH"] || "";
+const APICURIO_DESIGNER_API_URL=process.env["APICURIO_DESIGNER_API_URL"] || "/apis/designer/v0";
+const SHOW_MASTHEAD=process.env["APICURIO_SHOW_MASTHEAD"] || "true";
+const MASTHEAD_LABEL=process.env["APICURIO_MASTHEAD_LABEL"] || "API DESIGNER";
+const EDITORS_URL=process.env["APICURIO_EDITORS_URL"] || "/editors/";
 
-const AUTH_TYPE=process.env["DESIGNER_AUTH_TYPE"] || "none";
-const AUTH_URL=process.env["DESIGNER_AUTH_URL"] || "";
-const AUTH_CLIENT_ID=process.env["DESIGNER_AUTH_CLIENT_ID"] || "api-designer-ui";
-const AUTH_REDIRECT_URL=process.env["DESIGNER_AUTH_REDIRECT_URL"] || "";
+const AUTH_TYPE=process.env["APICURIO_AUTH_TYPE"] || "none";
+const AUTH_URL=process.env["APICURIO_AUTH_URL"] || "";
+const AUTH_CLIENT_ID=process.env["APICURIO_AUTH_CLIENT_ID"] || "api-designer-ui";
+const AUTH_REDIRECT_URL=process.env["APICURIO_AUTH_REDIRECT_URL"] || "";
 
 // Create the config to output.
 const CONFIG = {
     "apis": {
-        "designer": DESIGNER_API_URL
+        "designer": APICURIO_DESIGNER_API_URL
     },
     "ui": {
         "contextPath": CONTEXT_PATH,
