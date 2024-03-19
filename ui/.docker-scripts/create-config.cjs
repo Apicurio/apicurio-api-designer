3fba85930440eb16776de06cf0865ade25b7a41b
Carles Arnal <carlesarnal92@gmail.com>
2024-03-19 15:23:46 +0100

    Rename UI properties

--- ui/.docker-scripts/create-config.cjs
+++ ui/.docker-scripts/create-config.cjs
@@ -13,10 +13,10 @@ const SHOW_MASTHEAD=process.env["DESIGNER_SHOW_MASTHEAD"] || "true";
 const MASTHEAD_LABEL=process.env["DESIGNER_MASTHEAD_LABEL"] || "API DESIGNER";
 const EDITORS_URL=process.env["DESIGNER_EDITORS_URL"] || "/editors/";
 
-const AUTH_TYPE=process.env["DESIGNER_AUTH_TYPE"] || "none";
-const AUTH_URL=process.env["DESIGNER_AUTH_URL"] || "";
-const AUTH_CLIENT_ID=process.env["DESIGNER_AUTH_CLIENT_ID"] || "api-designer-ui";
-const AUTH_REDIRECT_URL=process.env["DESIGNER_AUTH_REDIRECT_URL"] || "";
+const AUTH_TYPE=process.env["APICURIO_AUTH_TYPE"] || "none";
+const AUTH_URL=process.env["APICURIO_AUTH_URL"] || "";
+const AUTH_CLIENT_ID=process.env["APICURIO_AUTH_CLIENT_ID"] || "api-designer-ui";
+const AUTH_REDIRECT_URL=process.env["APICURIO_AUTH_REDIRECT_URL"] || "";
 
 // Create the config to output.
 const CONFIG = {
